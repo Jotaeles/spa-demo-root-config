2e5b734f1756d3ccdbec50a224556ed3647e6857
jotaeles <jl_sandoval_salas@hotmail.com>
2021-09-09 17:29:47 -0500

    add all files

---
+++ src/lernit-root-config.js
@@ -0,0 +1,23 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@lernit/spa-demo-header",
+  app: () => System.import("@lernit/spa-demo-header"),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@lernit/spa-demo-home",
+  app: () => System.import("@lernit/spa-demo-home"),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@lernit/spa-demo-about",
+  app: () => System.import("@lernit/spa-demo-about"),
+  activeWhen: ["/about"],
+});
+
+start({
+  urlRerouteOnly: true,
+});
